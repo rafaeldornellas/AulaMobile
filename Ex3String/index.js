99253fe89f2a0a38ebc33133772db11f1b8d808a
rafaeldornellas <142440880+rafaeldornellas@users.noreply.github.com>
2025-02-22 09:52:53 -0300

    Add files via upload

---
+++ Ex3String/index.js
@@ -0,0 +1,14 @@
+let pais = "Brasil";
+let continente = "América do Sul";
+
+console.log(pais);//->Brasil
+console.log(typeof pais);//->String
+console.log(continente);//-> América do Sul
+console.log(typeof continente);//-> string
+
+let message1 = "O navio 'mars' fez escala no porto";
+let message2 = 'Chuva forte o navio partiu';
+
+console.log(message1);
+console.log(message2);
+console.log(message1 + message2);
---
+++ Ex4TempLiteral/index.js
@@ -0,0 +1,7 @@
+let nome = "João";
+let idade = 30;
+
+//Usando template literals
+let mensagem = `Olá, ${nome}! Você tem ${idade} anos.`;
+console.log(mensagem);
+//Exibe: Olá, oão! Você tem 30 anos.
