f0a0d5eaeb4eed396543ccc9ef99c8ec97e3c88e
rafaeldornellas <142440880+rafaeldornellas@users.noreply.github.com>
2025-02-22 10:53:24 -0300

    Add files via upload

---
+++ Ex12ObjFun/index.js
@@ -0,0 +1,8 @@
+let carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  acelerar: function() {
+    console.log("O carro está acelerando!");
+  }
+};
+carro.acelerar();//Exibe: O carro está a acelerando!
